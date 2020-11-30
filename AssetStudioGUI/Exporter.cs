f8aa975fe6b9aa7965b3654afd745b3eb6fbedcb
Gio <sethg@ipi.org>
2020-11-29 18:44:54 -0600

    Always include uniqueID

--- AssetStudioGUI/Exporter.cs
+++ AssetStudioGUI/Exporter.cs
@@ -301,12 +301,12 @@ namespace AssetStudioGUI
         private static bool TryExportFile(string dir, AssetItem item, string extension, out string fullPath)
         {
             var fileName = FixFileName(item.Text);
-            fullPath = Path.Combine(dir, fileName + extension);
-            if (!File.Exists(fullPath))
-            {
-                Directory.CreateDirectory(dir);
-                return true;
-            }
+            // fullPath = Path.Combine(dir, fileName + extension);
+            // if (!File.Exists(fullPath))
+            // {
+            // Directory.CreateDirectory(dir);
+            // return true;
+            // }
             fullPath = Path.Combine(dir, fileName + item.UniqueID + extension);
             if (!File.Exists(fullPath))
             {
